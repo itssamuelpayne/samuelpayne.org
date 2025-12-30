4a4f76fa323e4e2f5cccebf1a53b45d3429a0643
Samuel Payne <sam@wonderful.studio>
2025-12-30 08:12:34 -0500

    Update homepage copy

--- src/app/HomePage.tsx
+++ src/app/HomePage.tsx
@@ -14,7 +14,7 @@ export function HomePage({ onArticlesLoad }: HomePageProps) {
       <main>
         <Hero 
           intro="Sam leads high-performing design teams in complex organizations—he helps them do the best work of their lives."
-          bio="His experience spans AI, product design, and innovation strategy, ranging from products serving billions of uses to scrappy 0-1 prototypes. He's happiest when he's close to the work, pushing to simplify and make even the most complex technology easy to use. He cultivates diverse, resilient organizations because he focuses on the work (and most good things flow from that).\n\nSam is an active writer and speaker at the intersection of design and AI. His work has been recognized by D&AD, Fast Company, and Time Magazine."
+          bio="His experience spans AI, product design, and innovation strategy, ranging from products serving billions of users to scrappy 0-1 prototypes. He's happiest when he's close to the work, pushing to simplify and make even the most complex technology easy to use. He cultivates diverse, resilient organizations because he focuses on the work (and most good things flow from that).\n\nSam is an active writer and speaker at the intersection of design and AI. His work has been recognized by D&AD, Fast Company, and Time Magazine."
         />
         <BlogList mediumUsername="@itssamuelpayne" onArticlesLoad={onArticlesLoad} />
       </main>
